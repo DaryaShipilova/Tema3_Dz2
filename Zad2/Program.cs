e45f7262b3b3d804bea8a27d381ded58cc370818
Darya <dashulya18@mail.ru>
2022-08-16 19:51:35 +0300

    Домашнее задание 2.Тема3

---
+++ Zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет (645 -> 5,  78 -> третьей цифры нет, 32679 -> 6). 
+
+Console.WriteLine("Введите любое число ");
+string number1 = Console.ReadLine();
+int length = number1.Length;
+if(length >= 3)
+{ 
+int one = Int32.Parse(number1);
+Console.Clear();
+Console.WriteLine("Третяя цифра — " + number1[2]);
+}
+else
+{
+Console.WriteLine("Третей цифры нет, попробуйте ввести другое число"); 
+}
+ 
---
+++ Zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным (Пример: 6 -> да, 7 -> да, 1 -> нет). 
+
+Console.WriteLine ("Введите число от 1 до 7");
+int number = Convert.ToInt32 (Console.ReadLine ());
+if (number == 6)
+{
+    Console.WriteLine ("Да");
+}
+else if (number == 7)
+{
+   Console.WriteLine ("Да"); 
+}
+else 
+if (number > 0)
+{   
+if (number < 6)
+{
+    Console.WriteLine ("Нет"); 
+}
+    else
+{
+    Console.WriteLine ("Число введено неверно. Введите число от 1 до 7");
+}
+} 
